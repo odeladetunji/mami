9982cd674e1e5c5deeabf4d47944b62d5a6bd48b
unknown <odeladetunji@gmail.com>
2018-04-12 08:16:20 +0100

    comit

--- index.js
+++ index.js
@@ -6,24 +6,27 @@ var server = require('http').Server(app);
 var io = require('socket.io').listen(server);
 var path = require('path');
 var mysql = require('mysql');
-var bodyparser = require('body-parser');
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json()
 var cookieParser = require('cookie-parser');
 var cors = require('cors');
 var $ = require("jquery");
 var landing = require('./routes/homepage');
 var mongo = require('mongodb');
 // bodyparser middleware!!!
-var bodyParser = require('body-parser');
 var homepage = require('./routes/homepage');
 var livestream = require('./routes/livestream');
 var homepageFromLiveStream = require('./routes/homepage');
 var admin = require('./routes/admin');
+var adminAuthentication = require('./routes/adminAuthentication');
+var authenticateAdmin = require('./routes/authenticateAdmin');
+var checkIfAdmin = require('./routes/checkIfAdmin');
+
 /*
 modules to handle navigations within the site
 */
 //var get_Peoplepage = require('./routes/getPeoplepage');
 //var get_Grouppage = require('./routes/getGrouppage');
-
 app.use('*', cors());
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -31,7 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
-app.use(bodyParser.json());
+app.use(bodyParser.json({type: 'application/json'}));
 app.use(cookieParser());
 //app.options(cors());
 app.set('view engine', 'ejs');
@@ -39,18 +42,23 @@ app.use('/', homepage);
 app.use('/livestream', livestream);
 app.use('/homepage', homepageFromLiveStream);
 app.use('/admin', admin);
+app.use('/adminAuthentication', adminAuthentication);
+app.use('/authenticateAdmin', authenticateAdmin);
+app.use('/checkIfAdmin', checkIfAdmin);
 
 /*var bodyParser = require('body-parser');
-// setting mysql variable
+// setting mysql variable */
 var connection = mysql.createConnection({
     host     : '127.0.0.1',
     user     : 'root',
-    password : 'root',
+    password : '',
     database : 'lovedaises'
 });
 
 // connecting to mysql server
-connection.connect();*/
+connection.connect();
+
+
 
 // listening on port 9000...
 /*app.listen(9000, function () {
