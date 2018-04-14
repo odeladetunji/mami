154398b6611fe046bd221e41782802a7e1b9d54a
unknown <odeladetunji@gmail.com>
2018-04-14 01:38:17 +0100

    commit

--- routes/mamiupdate.js
+++ routes/mamiupdate.js
@@ -74,6 +74,132 @@ router.post('/', function(req, res){
 		 return;
     }
 
+    if(message == 'freedomonair'){
+         function updateFreedomOnAir(){
+         	    data1 = req.body.firstData;
+		   	    data2 = req.body.secondData;
+		   	    data3 = req.body.thirdData;
+		   	    console.log(data1);
+		   	    sql = 'INSERT INTO mamicontent SET freedomonair = ?, freedomonairdiscription = ?, freedomonairsubdiscription = ?';
+		        connection.query(sql, [data1, data2, data3], function(error, results, fields){
+		            if(error) throw error;
+		            res.send({'message': 'ok'});
+		        });
+         }
+
+         updateFreedomOnAir();
+         return;
+    }
+
+    if(message == 'LoveChoices'){
+          function updateLoveChoices(){
+          	    data1 = req.body.firstData;
+		   	    data2 = req.body.secondData;
+		   	    sql = 'INSERT INTO mamicontent SET lovechoices = ?, lovechoicesdescription = ?';
+		        connection.query(sql, [data1, data2], function(error, results, fields){
+		            if(error) throw error;
+		            res.send({'message': 'ok'});
+		        });
+          }
+
+          updateLoveChoices();
+          return;
+    }
+
+     if(message == 'ministersin'){
+          function updateMinistersnIn(){
+          	    data1 = req.body.firstData;
+		   	    data2 = req.body.secondData;
+		   	    sql = 'INSERT INTO mamicontent SET ministersin = ?, ministersindescription = ?';
+		        connection.query(sql, [data1, data2], function(error, results, fields){
+		            if(error) throw error;
+		            res.send({'message': 'ok'});
+		        });
+          }
+
+          updateMinistersnIn();
+          return;
+    }
+
+    if(message == 'freedomministry'){
+          function updateFreedomMinistry(){
+          	    data1 = req.body.firstData;
+		   	    data2 = req.body.secondData;
+		   	    sql = 'INSERT INTO mamicontent SET freedomministry = ?, freedomministrydiscription = ?';
+		        connection.query(sql, [data1, data2], function(error, results, fields){
+		            if(error) throw error;
+		            res.send({'message': 'ok'});
+		        });
+          }
+
+          updateFreedomMinistry();
+          return;
+    }
+    
+    if(message == 'publishingarm'){
+          function updatePublishingArm(){
+          	    data1 = req.body.firstData;
+		   	    data2 = req.body.secondData;
+		   	    sql = 'INSERT INTO mamicontent SET publishingarm = ?, publishingarmdiscription = ?';
+		        connection.query(sql, [data1, data2], function(error, results, fields){
+		            if(error) throw error;
+		            res.send({'message': 'ok'});
+		        });
+          }
+
+          updatePublishingArm();
+          return;
+    }
+
+    if(message == 'reviveministry'){
+          function updateReviveMinistry(){
+          	    data1 = req.body.firstData;
+		   	    data2 = req.body.secondData;
+		   	    sql = 'INSERT INTO mamicontent SET reviveministry = ?, reviveministrydiscription = ?';
+		        connection.query(sql, [data1, data2], function(error, results, fields){
+		            if(error) throw error;
+		            res.send({'message': 'ok'});
+		        });
+          }
+
+          updateReviveMinistry();
+          return;
+    }
+
+    if(message == 'mamipartners'){
+          function updatePartnership(){
+          	    data1 = req.body.firstData;
+		   	    data2 = req.body.secondData;
+		   	    data3 = req.body.thirdData;
+		   	    sql = 'INSERT INTO mamicontent SET mamipartners = ?, mamipartnersubheading = ?, mamipartnerdiscription = ?';
+		        connection.query(sql, [data1, data2, data3], function(error, results, fields){
+		            if(error) throw error;
+		            res.send({'message': 'ok'});
+		        });
+          }
+
+          updatePartnership();
+          return;
+    }
+
+    if(message == 'onlinepayment'){
+          function updateOnlinePayment(){
+          	    data1 = req.body.firstData;
+		   	    data2 = req.body.secondData;
+		   	    data3 = req.body.thirdData;
+		   	    data4 = req.body.fouthData;
+		   	    data5 = req.body.fifthData;
+		   	    sql = 'INSERT INTO mamicontent SET mamipartners = ?, mamipartnersubheading = ?, mamipartnerdiscription = ?';
+		        connection.query(sql, [data1, data2, data3, data4, data5], function(error, results, fields){
+		            if(error) throw error;
+		            res.send({'message': 'ok'});
+		        });
+          }
+
+          updateOnlinePayment();
+          return;
+    }
+    
     console.log('Got to this point');
 });
 
