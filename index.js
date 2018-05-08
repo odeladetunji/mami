0eace0a8f086e1082372fcd23cfb6cd31f17a188
unknown <odeladetunji@gmail.com>
2018-05-08 08:13:35 +0100

    commit

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ var jsonParser = bodyParser.json()
 var cookieParser = require('cookie-parser');
 var cors = require('cors');
 var $ = require("jquery");
+var multer = require('multer');
+var fs = require('fs');
 var landing = require('./routes/homepage');
 var mongo = require('mongodb');
 // bodyparser middleware!!!
@@ -25,17 +27,46 @@ var bs = require("browser-sync").create();
 var gettingAdminContent = require('./routes/gettingAdminContent');
 var mamiupdate = require('./routes/mamiupdate');
 var gettingMamiContent = require('./routes/gettingMamiContent');
+
+var dir = path.join(__dirname, 'public');
+
+var mime = {
+    html: 'text/html',
+    txt: 'text/plain',
+    css: 'text/css',
+    gif: 'image/gif',
+    jpg: 'image/jpeg',
+    png: 'image/png',
+    svg: 'image/svg+xml',
+    js: 'application/javascript'
+};
+
+var storage =   multer.diskStorage({
+      destination: function (req, file, callback) {
+      callback(null, './public/images/');
+      },
+      filename: function (req, file, callback) {
+      // callback(null, file.fieldname + '-' + Date.now());
+      callback(null, file.originalname);
+      //email = req.body.name;
+      }
+    });
+
+var upload = multer({ storage : storage,
+                      limits: {fileSize: 1000000} }).single('imagefile');
+
 /*
 modules to handle navigations within the site
 */
 //var get_Peoplepage = require('./routes/getPeoplepage');
 //var get_Grouppage = require('./routes/getGrouppage');
+
+
 app.use('*', cors());
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
-
 // parse application/json
 app.use(bodyParser.json({type: 'application/json'}));
 app.use(cookieParser());
@@ -51,9 +82,11 @@ app.use('/checkIfAdmin', checkIfAdmin);
 app.use('/gettingAdminContent', gettingAdminContent);
 app.use('/mamiupdate', mamiupdate);
 app.use('/gettingMamiContent', gettingMamiContent);
+//app.use('/searchAlgorithm', searchAlgorithm);
 
-/*var bodyParser = require('body-parser');
 // setting mysql variable */
+
+
 var connection = mysql.createConnection({
     host     : '127.0.0.1',
     user     : 'root',
@@ -64,24 +97,39 @@ var connection = mysql.createConnection({
 // connecting to mysql server
 connection.connect();
 
+app.get('*', function (req, res) {
+    var file = path.join(dir, req.path.replace(/\/$/, '/index.html'));
+    if (file.indexOf(dir + path.sep) !== 0) {
+        return res.status(403).end('Forbidden');
+    }
+    var type = mime[path.extname(file).slice(1)] || 'text/plain';
+    var s = fs.createReadStream(file);
+    s.on('open', function () {
+        res.set('Content-Type', type);
+        s.pipe(res);
+    });
+    s.on('error', function () {
+        res.set('Content-Type', 'text/plain');
+        res.status(404).end('Not found');
+    });
+});
 
+app.post('/mamiupdatetest', function(req, res){
+      upload(req, res, function(){
+        console.log(req.files);
+        console.log(req.body)
+        res.send(req.body);
+      });
+});
 
 // listening on port 9000...
 /*app.listen(9000, function () {
  console.log('Our app is still working!')
 })*/
 
+
 server.listen(5000, function(){
   console.log("Mami is now running live on Port:   5000");
 });
-
-
-/*bs.reload('*.css');
-bs.reload('*.ejs');
-bs.reload('*.js');
-bs.init({
-	server: 'server',
-	port: 5000
-});*/
 
 module.export = app;
